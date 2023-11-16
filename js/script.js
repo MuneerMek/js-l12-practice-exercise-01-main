c9437e19a57a5de943e3c3821acb51ab8cb9f0c7
M_Mek <mmekraz781@gmail.com>
2023-11-16 14:23:04 -0500

    added label w/ selector

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-  const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+  const usersRequest = await fetch(
+    `https://randomuser.me/api?results=${numUsers}`
+  );
   const data = await usersRequest.json();
   const userResults = data.results;
   displayUsers(userResults);
@@ -22,4 +25,9 @@ const displayUsers = function (userResults) {
   }
 };
 
-getData();
+selectUserNumber.addEventListener("change", function (e) {
+  numUsers = e.target.value;
+  getData(numUsers);
+});
+
+getData(1);
